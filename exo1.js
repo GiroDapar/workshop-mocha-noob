e794b5a9482ec6d7830b50b18204a0bffa8516c2
GiroDapar <romaingirard97@gmail.com>
2017-11-21 15:30:05 +0100

    exercice1

---
+++ exo1.js
@@ -0,0 +1,9 @@
+var expect = require("chai").expect;
+
+var getprices = require("../src/shoptools").getprices;
+
+describe("getprices", function () {
+    var products = getprices();
+
+    itcvnvbnf
+})
